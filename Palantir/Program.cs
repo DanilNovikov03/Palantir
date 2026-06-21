af97949e7020a568075f8db230e12376db8eafe8
3Litra <danil.novikov.0305@gmail.com>
2026-06-21 18:27:34 +0700

    chore: swagger is not launched

--- Palantir/Program.cs
+++ Palantir/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddControllers()
             new NetTopologySuite.IO.Converters.GeoJsonConverterFactory()
         );
     });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
